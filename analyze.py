10f3bc3319d8a955f175315d66477831de878d2c
Guilherme Gomes <gui.rock08@gmail.com>
2024-02-09 23:56:38 -0300

    Adding date column

--- analyze.py
+++ analyze.py
@@ -12,7 +12,6 @@ load_dotenv()
 
 def calc_variation(today_price, yesterday_price, week_price, year_price):   
     variation_total = ((today_price - yesterday_price) * 2) + ((today_price - week_price) * 2.5) + ((today_price - year_price) * 3)
-    
     return variation_total
 
 def check_coin_existence(coin_pair):
@@ -51,13 +50,23 @@ def check_coins():
 
 def download_info_coin(start, end):
     acronyms = check_coins()
+    csv_file_path = 'currency_data.csv'
     
+    if os.path.exists(csv_file_path):
+        existing_data = pd.read_csv(csv_file_path)
+        last_date = existing_data['Date'].max()
+    else:
+        existing_data = pd.DataFrame(columns=['Coin', 'Date', 'Volume', 'High', 'Low'])
+        last_date = start - pd.DateOffset(days=1)  # Inicializa a última data com um dia antes do início
+
     df_list = []
 
     for acronym in acronyms:
         try:
-            data = yf.download(acronym.replace("USD", "-USD"), start=start, end=end)
+            data = yf.download(acronym.replace("USD", "-USD"), start=last_date, end=end)
             data['Coin'] = acronym
+            data.reset_index(inplace=True)  # Resetando o índice para a data se tornar uma coluna
+            data = data[['Coin', 'Date', 'Volume', 'High', 'Low']]
             df_list.append(data)
             print(f"Dados obtidos para {acronym}.")
         except Exception as e:
@@ -66,10 +75,11 @@ def download_info_coin(start, end):
         time.sleep(1)
 
     if len(df_list) > 0:
-        df = pd.concat(df_list)
-        df.to_csv('currency_data.csv', index=False)
-        return df
+        new_data = pd.concat(df_list)
+        merged_data = pd.concat([existing_data, new_data])
+        merged_data.to_csv(csv_file_path, index=False)
+        return merged_data
     else:
-        return None
+        return existing_data
     
 download_info_coin(datetime(2024, 2, 4), datetime.now().date().isoformat())
